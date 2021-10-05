376475ae5ced617ab324368c8778f252222293ad
Kateryna Karpova <katyakarpova@gmail.com>
2021-10-05 10:55:52 +0200

    Back to classes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
-import { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import ContactsList from './ContactList/ContactsList';
 import Filter from './Filter/Filter';
-import InputForm from './Forms/InputForm';
+import Form from './Forms/Form';
 
-export default function App() {
-  const [contacts, setContacts] = useState(
-    () =>
-      JSON.parse(window.localStorage.getItem('Contacts')) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-  );
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
-  const [filter, setFilter] = useState('');
-
-  const formSubmitHandler = (name, number) => {
-    if (contacts.some(contact => contact.name === name)) {
-      alert(`${name} already exist in contact list`);
+  formSubmitHandler = data => {
+    if (this.state.contacts.some(contact => contact.name === data.name)) {
+      alert(`${data.name} already exist in contact list`);
       return;
     }
 
     const contact = {
       id: uuidv4(),
-      name: name,
-      number: number,
+      name: data.name,
+      number: data.number,
     };
 
-    setContacts(prevState => [contact, ...prevState]);
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
   };
 
-  const changeFilter = event => {
-    setFilter(event.currentTarget.value);
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
   };
 
-  const getVisibleContacts = () => {
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
     const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
@@ -45,32 +46,56 @@ export default function App() {
     );
   };
 
-  const handleDeleteContact = event => {
-    setContacts(
-      contacts.filter(contact => contact.id !== event.currentTarget.id),
-    );
+  handleDeleteContact = event => {
+    this.setState({
+      contacts: this.state.contacts.filter(
+        contact => contact.id !== event.currentTarget.id,
+      ),
+    });
   };
 
-  useEffect(() => {
-    localStorage.setItem('Contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  componentDidMount() {
+    console.log('App componentDidMount');
 
-  return (
-    <div className="container">
-      <div className="formsContainer">
-        <InputForm onSubmit={formSubmitHandler} />
-      </div>
+    const contacts = localStorage.getItem('Contacts');
+    const parsedContacts = JSON.parse(contacts);
+    console.log(parsedContacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
-      <div className="contactsContainer">
-        <div className="contacts">
-          <h2>Contacts</h2>
-          <Filter value={filter} onChange={changeFilter} />
-          <ContactsList
-            contacts={getVisibleContacts()}
-            onClick={handleDeleteContact}
-          />
+  componentDidUpdate(prevProps, prevState) {
+    console.log('App componentDidUpdate');
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('Обновился список контактов');
+
+      localStorage.setItem('Contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const filteredContacts = this.getVisibleContacts();
+
+    return (
+      <div className="container">
+        <div className="formsContainer">
+          <Form onSubmit={this.formSubmitHandler} />
+        </div>
+
+        <div className="contactsContainer">
+          <div className="contacts">
+            <h2>Contacts</h2>
+            <Filter value={this.state.filter} onChange={this.changeFilter} />
+            <ContactsList
+              contacts={filteredContacts}
+              onClick={this.handleDeleteContact}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default App;
---
+++ src/Forms/Form.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+//import { v4 as uuidv4 } from 'uuid';
+
+class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state);
+
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form">
+        <label>
+          <h2>Name</h2>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          <h2>Number</h2>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit" className="button">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
